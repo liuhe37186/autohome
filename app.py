b2aeadf56fefc139708e8318c1d4c5a13ac7c9cf
liuhe <liuhe3716@163.com>
2018-08-28 14:21:55 +0800

    抓取具体车型信息

--- app.py
+++ app.py
@@ -3,9 +3,10 @@
 # from lib import get_cars
 import requests
 from bs4 import BeautifulSoup
-from carsource import get_car,get_car_detail
+from carsource import get_car,get_car_type
 from setting import headers, domain, start_url, file_output
 import json
+import bs4
 
 # output_file = open(file_output, 'w+',encoding='utf-8')
 
@@ -17,42 +18,19 @@ result = requests.get(start_url, headers=headers)
 html_content = result.content
 html_content_soup = BeautifulSoup(html_content,'html5lib')
 brands_tag =  html_content_soup.find_all('li')
-# series_tag = html_content_soup.find_all('a')
 
-# for series in series_tag:
-
-    # print(series.get_text().split('(')[0])
-     # print(series.get_text())
 
 def main():
-    brand_tag = brands_tag[4]
-    cars = []
-    brand_name  = brand_tag.get_text(',').split(',')[0]
-    brand_id = brand_tag.get('id')[1:]
-    brand_href = domain + brand_tag.a['href']
-    print(brand_name,brand_id,brand_href)
-    get_car(brand_name,brand_id)
-    # get_car_detail('https://car.autohome.com.cn/price/series-3170.html')
-
-# main()
-for brand_tag in brands_tag:
-
-    # print(brand_tag)
+    # url='https://car.autohome.com.cn/price/series-4851.html'
+    # get_car_type(url)
+    for brand_tag in brands_tag:
     
-    car = {}
-    brand_name  = brand_tag.get_text(',').split(',')[0]
-    brand_id = brand_tag.get('id')[1:]
-    brand_href = domain + brand_tag.a['href']
-    print(brand_name,brand_id,brand_href)
-    get_car(brand_name, brand_id)
-    # print(car)
-    # 输出中文问题
-    # for car in cars:
-    # line = json.dumps(car, indent=2,ensure_ascii=False)
+        car = {}
+        brand_name  = brand_tag.get_text(',').split(',')[0]
+        brand_id = brand_tag.get('id')[1:]
+        brand_href = domain + brand_tag.a['href']
+        print(brand_name,brand_id,brand_href)
+        get_car(brand_name, brand_id)
 
-    # print(line+'\n')
-    #     # 输出 Unicode 到文件
-  
-    # output_file.write(line)
-    # output_file.write('\n')
-      
+main()
+
--- carsource.py
+++ carsource.py
@@ -30,7 +30,7 @@ def get_car(brand_name,brand_id):
 				if child.name == 'dt':
 					land = child.get_text();
 				else:
-					car['brand_name'] = brand_name
+					car['brandName'] = brand_name
 					car['land'] = land
 					car['series'] = child.a.get_text()
 					car['href'] = domain+child.a.attrs['href']
@@ -47,49 +47,61 @@ def get_car(brand_name,brand_id):
                 		# 直接空格无效，因为gbk无法转换'\xa0'字符(http://www.educity.cn/wenda/350839.html)
 						car_attr_key = car_attr_key.replace(u'\xa0', '')
 						car[car_attr_key] = car_attr_value.strip(',')
-						cars.append(car)
-						line = json.dumps(car, indent=2,ensure_ascii=False)
+					car['carType'] = get_car_type(car['href'])
+					cars.append(car)
+					line = json.dumps(car, indent=2,ensure_ascii=False)
 
-						print(line+'\n')
-						# 输出 Unicode 到文件
+					print(line+'\n')
+					# 输出 Unicode 到文件
   
-						output_file.write(line)
-						output_file.write('\n')
+					output_file.write(line)
+					output_file.write('\n')
 					# car_all = get_car_detail(car['href'],car)
 					# return car_all
 					# print(cars)
 					
 
 	# soup.prettify()
-
-	# print(soup.prettify())
-def get_car_detail(car_detail_url,car):
-
-	result = requests.get(car_detail_url,headers=headers)
-	soup = BeautifulSoup(result.content,'html5lib')
-	car_details = soup.find(class_='list-cont-main')
-	car['price'] = car_details.find(class_='font-arial').get_text(strip=True)
-	for car_attr_tag  in car_details.find('ul', class_='lever-ul').find_all('li'):
-		car_attr = car_attr_tag.get_text(',',strip=True)
-		if len(car_attr.split(u'：')) < 2 :
-			continue
-		car_attr_key = car_attr.split(u'：')[0]
-		car_attr_value = car_attr.split(u'：')[1]
-                # 直接空格无效，因为gbk无法转换'\xa0'字符(http://www.educity.cn/wenda/350839.html)
-		car_attr_key = car_attr_key.replace(u'\xa0', '')
-		car[car_attr_key] = car_attr_value.strip(',')
-		print(car)
-		# return car
-	# print(car)
+def get_car_type(series_url):
+	# url='https://car.autohome.com.cn/price/series-4851.html'
+	res= requests.get(series_url,headers=headers)
+	soup = BeautifulSoup(res.content,'html5lib')
+    # print(soup.prettify())
+	car_details = soup.find(id='divSeries')
+	car_type_list = []
 	
-	
-	# for tag in car_details.children:
-		# car['级别']=tag.find(class_='info-gray').get_text()
-		# print(car)
-		# tag.prettify()
-		# print(tag.find_all('li'))
+	engine = ''
+    # print(car_details.prettify())
 
-	# print(car_details.prettify())
+	for tag in car_details.children:
+
+		if isinstance(tag.find('ul'),bs4.element.Tag):
+			for li in tag.find('ul').find_all('li'):
+				car_type={}
+				car_type['车型'] = li.find('div',class_='interval01-list-cars').p.get_text()
+				print(li.find('div',class_='interval01-list-cars').p.get_text())
+				for p in li.find('div',class_='interval01-list-cars').find_all('p'):
+					spanList=[]
+					spanStr={}
+					if p.span != None:
+						for span in p.children: 
+							spanList.append(span.get_text())
+                        # print(spanList)
+						if len(spanList) > 0:	
+							car_type['驱动']= spanList[0]
+						elif len(spanList) > 1:
+							car_type['变速箱'] = spanList[1]
+                # print(li.find('div',class_='interval01-list-cars').p.next_slibing)
+                # if li.find('div',class_='interval01-list-cars').p.span != None:
+                    # print(li.find('div',class_='interval01-list-cars').p.span.get_text())
+				car_type['指导价']= li.find('div',class_='interval01-list-guidance').get_text()
+				# print(li.find('div',class_='interval01-list-guidance').get_text())
+				# print(car_type)
+				# print(car_type)
+				car_type_list.append(car_type)
+				# print(car_type_list)
+	print(car_type_list)
+	return car_type_list
 
 
 
